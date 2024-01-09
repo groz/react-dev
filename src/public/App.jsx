a302385c8cb85f82cfe3ea26360ee6ea66a5ff86
Tagir <tagir.magomedov@gmail.com>
2024-01-10 00:55:36 +0100

    Add CSS.  Remove race condition in hotreload startup.

--- src/public/App.jsx
+++ src/public/App.jsx
@@ -58,7 +58,7 @@ function Status({ turn, isGameOver }) {
 }
 
 function Square({ value, onClick }) {
-  return <button onClick={onClick}>{value}</button>;
+  return <button onClick={onClick} className="square">{value}</button>;
 }
 
 function Game() {
@@ -95,17 +95,17 @@ function Game() {
       <div>
         <Status turn={turn} isGameOver={calculateGameOver(squares)} />
       </div>
-      <div>
+      <div className="board-row">
         <Square onClick={() => handleClick(0)} value={squares[0]} />
         <Square onClick={() => handleClick(1)} value={squares[1]} />
         <Square onClick={() => handleClick(2)} value={squares[2]} />
       </div>
-      <div>
+      <div className="board-row">
         <Square onClick={() => handleClick(3)} value={squares[3]} />
         <Square onClick={() => handleClick(4)} value={squares[4]} />
         <Square onClick={() => handleClick(5)} value={squares[5]} />
       </div>
-      <div>
+      <div className="board-row">
         <Square onClick={() => handleClick(6)} value={squares[6]} />
         <Square onClick={() => handleClick(7)} value={squares[7]} />
         <Square onClick={() => handleClick(8)} value={squares[8]} />
--- src/server.js
+++ src/server.js
@@ -7,32 +7,34 @@ const app = express();
 const port = 3000;
 const esbuild_port = 9000;
 
-const publicPath = path.join(__dirname, "/public");
+setupCompression(app);
 
-// Compression middleware doesn't work with SSE.
-// See: https://github.com/expressjs/compression#server-sent-events
-DEV: app.use(
-  compression({
-    filter: function excludeHotReload(req, res) {
-      if (req.url.includes("/esbuild")) {
-        return false;
-      }
-      return compression.filter(req, res);
-    },
-  })
-);
-PROD: app.use(compression());
-
-// Note: define app routes before proxy setup.
-app.get("/what", (req, res) => {
-  res.send("Hello World - updated!!!");
+// Routes should be defined before proxy setup.
+app.get("/api", (req, res) => {
+  res.json([1, 2, 3, {"a": "b"}]);
 });
 
 // Proxy client requests to esbuild in dev mode.
 DEV: app.use("/", createProxyMiddleware(`http://localhost:${esbuild_port}`));
-PROD: app.use(express.static(publicPath));
+PROD: app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
   DEV: console.log("Development mode with live reload enabled.");
 });
+
+function setupCompression(app) {
+  // Compression middleware doesn't work with SSE.
+  // See: https://github.com/expressjs/compression#server-sent-events
+  DEV: app.use(
+    compression({
+      filter: function excludeHotReload(req, res) {
+        if (req.url.includes("/esbuild")) {
+          return false;
+        }
+        return compression.filter(req, res);
+      },
+    }),
+  );
+  PROD: app.use(compression());
+}
